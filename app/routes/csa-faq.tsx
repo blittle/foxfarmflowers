1ec981f3765a044c9cd40b880099111dcf960d3d
Bret Little <bret.little@gmail.com>
2023-02-18 11:25:57 -0500

    Update

---
+++ app/routes/csa-faq.tsx
@@ -0,0 +1,14 @@
+import CSAFaq from "~/components/CSAFaq";
+
+export default function FAQ() {
+  return (
+    <>
+      <img
+        className="w-full"
+        src="https://cdn.shopify.com/s/files/1/0712/2911/2602/files/SAF03539_1024x274.jpg?v=1675027632"
+        alt="flowers"
+      />
+      <CSAFaq />
+    </>
+  );
+}
