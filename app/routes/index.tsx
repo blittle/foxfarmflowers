5b2350fd616e61c4e1d05db50357caa5691a0653
Bret Little <bret.little@gmail.com>
2023-05-09 22:29:39 -0400

    Update

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -72,11 +72,11 @@ export default function Index() {
           style={{ width: 190 }}
         >
           <div className="montagu text-5xl fox-green" style={{ fontSize: 26 }}>
-            2023 Bouquet CSA Preorder is Now Open!
+            Mother's Day Bouquets are now available!
           </div>
           <a
             className="rounded mt-4 py-2 bg-fox-green text-white block"
-            href="/shop"
+            href="/products/mothers-day-bouquets"
           >
             Shop Now
           </a>
